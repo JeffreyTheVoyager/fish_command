9fccee42b1cc35935c912583743caf505e35a3b0
Jeffrey Abraham <Jeffreyabraham27@gmail.com>
2022-11-06 16:29:08 -0500

    Move file to fish functions to run in terminal

--- weight_converter.fish
+++ weight_converter.fish
@@ -4,9 +4,9 @@ function convert --argument unitOne unitTwo weight
      if test $unitOne = 'kg'; and test $unitTwo = 'lb'
           echo (math "$weight * 2.205")
      end
+
      if test $unitOne = 'lb'; and test $unitTwo = 'kg'
           echo (math "$weight / 2.205 ")
      end
 end
-
-convert
+ 
